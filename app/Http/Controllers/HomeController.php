8af7160706e52d6de92fceb82ea541433f116cfa
Akshay Singh Rajput <71835487+akshayhere-004@users.noreply.github.com>
2021-08-31 09:58:19 +0100

    comments added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Mail;
 
 class HomeController extends Controller
 {
-    /**
+     /**
      * Create a new controller instance.
      *
      * @return void
@@ -19,7 +19,7 @@ class HomeController extends Controller
         $this->middleware(['auth','verified']);
     }
 
-    /**
+     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
@@ -29,6 +29,11 @@ class HomeController extends Controller
         return view('home');
     }
 
+     /**
+     * Gets all the plants from plants data base
+     *
+     * @return plants which are not selected by the user
+     */
     public function selectplants()
     {
         $ids = DB::table('my_plants')->where('user_id',Auth()->user()->id)->pluck('plant_id');
@@ -36,6 +41,12 @@ class HomeController extends Controller
         return view('select_plants')->with('plants',$plants);
     }
 
+     /**
+     * To add the selected plant to the users plant collection
+     *
+     * @param  mixed $request contains currently logged in userId
+     * @return redirects back to the select plant view
+     */
     public function selectplant(Request $request)
     {
         $id = $request->input('id');
@@ -49,6 +60,11 @@ class HomeController extends Controller
         return redirect()->back();
     }
 
+     /**
+     * To find all the plants data which user has
+     *
+     * @return plants of a user
+     */
     public function myplants()
     {
         $ids = DB::table('my_plants')->where('user_id',Auth()->user()->id)->pluck('plant_id');
@@ -56,6 +72,12 @@ class HomeController extends Controller
         return view('my_plants')->with('plants',$plants);
     }
 
+     /**
+     * To remove the plant from the user plants collection
+     *
+     * @param  mixed $request contains selected plantId to be removed
+     * @return redirects back to the myPlants view
+     */
     public function unselectplant(Request $request)
     {
         $id = $request->input('id');
@@ -63,6 +85,14 @@ class HomeController extends Controller
         return redirect()->back();
     }
 
+     /**
+     * To get the data of a user selected plant
+     * Also the information such as when was the last time water was given
+     * How many times in a week it needs water
+     * If it needs water then a button appears to remind user to give water
+     * @param  mixed $request containts plantId
+     * @return view containing plat information i.e., myPlant
+     */
     public function myplant(Request $request)
     {
         $period = DB::table('plant_data')->where('id',$request->input('id'))->pluck('water_duration')[0];
@@ -92,6 +122,12 @@ class HomeController extends Controller
         return view('plant_status')->with('plant',$data);
     }
 
+     /**
+     * To update the information of water has been given to the plants when a user marks the task complete
+     *
+     * @param  mixed $request contains plant ID
+     * @return redirect backs to the view containing plant information i.e., myPlant
+     */
     public function markwatered(Request $request)
     {
         $id = DB::table('my_plants')->where('plant_id',$request->input('id'))->where('user_id',Auth()->user()->id)->pluck('id')[0];
@@ -104,6 +140,12 @@ class HomeController extends Controller
          return redirect()->back();
       
     } 
+     
+     /**
+     * To send email to the user to give water to a plant
+     *
+     * @return void
+     */
     public function sendmail(){
         $user = ['user'=>'haiderjutt95@gmail.com'];
         Mail::send('emails', ['user' => $user], function ($m) use ($user) {
